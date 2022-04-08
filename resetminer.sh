538ebbdb56212c0d091808c74ed3e47aa1e80376
mhilgenkamp <mhilgenkamp@pruim.nl>
2022-04-08 18:13:34 +0200

    Added file operations

--- resetminer.sh
+++ resetminer.sh
@@ -7,3 +7,15 @@ fi
 cd /root
 wget https://idea-01.insufficient-light.com/data/thought-chain.tar.gz
 tar -zxvf thought-chain.tar.gz
+
+systemctl stop thoughtd
+rm /root/.thoughtcore/evodb/ -r
+rm /root/.thoughtcore/blocks/ -r
+rm /root/.thoughtcore/chainstate/ -r
+
+mv /root/evodb/ /root/.thoughtcore/evodb/ -r
+mv /root/blocks/ /root/.thoughtcore/blocks/ -r
+mv /root/chainstate/ /root/.thoughtcore/chainstate/ -r
+
+systemctl start thoughtd
+journalctl -30 -u thoughtd
