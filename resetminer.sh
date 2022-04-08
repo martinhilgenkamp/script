f73b705374804ff3119acd5db4cb7c22bf42c3fe
mhilgenkamp <mhilgenkamp@pruim.nl>
2022-04-08 18:37:25 +0200

    Added Feedback

--- resetminer.sh
+++ resetminer.sh
@@ -5,20 +5,23 @@ if [ "$EUID" -ne 0 ]
 fi
 
 cd /root
+echo "Downloading Bootstrap"
 wget http://192.168.250.65/thought-chain.tar.gz
-tar -zxvf thought-chain.tar.gz
-
+echo "Extracting Bootstrap"
+tar -zxf thought-chain.tar.gz
+echo "Stopping services"
 systemctl stop thoughtd
+echo "Removing Files"
 rm /root/.thoughtcore/evodb/ -r
 rm /root/.thoughtcore/blocks/ -r
 rm /root/.thoughtcore/chainstate/ -r
 
 echo "Old files removed"
-/bin/sleep 3
-
+echo "Rebuilding blockchain"
 mv /root/evodb/ /root/.thoughtcore/evodb/
 mv /root/blocks/ /root/.thoughtcore/blocks/
 mv /root/chainstate/ /root/.thoughtcore/chainstate/
-
+echo "Blockchain recovered"
+echo "Starting Thoughtd Service"
 systemctl start thoughtd
-journalctl -30 -u thoughtd
+echo "Thoughtd Service Started"
