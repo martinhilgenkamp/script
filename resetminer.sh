80610eec7b9187b59ebaae2c5cef98625a6efb17
martinhilgenkamp <31311307+martinhilgenkamp@users.noreply.github.com>
2023-03-02 08:43:04 +0100

    Update resetminer.sh
    Change the sequence of actions for usage on devices with low storage space.
    Chain will be removed before downloading the bootstrap file

--- resetminer.sh
+++ resetminer.sh
@@ -5,22 +5,25 @@ if [ "$EUID" -ne 0 ]
 fi
 
 cd /root
-echo "Downloading Bootstrap"
 
-wget https://idea-01.insufficient-light.com/data/thought-chain.tar.gz
-#wget http://192.168.250.65/thought-chain.tar.gz
+echo "Cleaning up"
+rm "*.tar.gz"
 
-
-echo "Extracting Bootstrap"
-tar -zxf thought-chain.tar.gz
 echo "Stopping services"
 systemctl stop thoughtd
-echo "Removing Files"
+echo "Removing chain Files"
 rm /root/.thoughtcore/evodb/ -r
 rm /root/.thoughtcore/blocks/ -r
 rm /root/.thoughtcore/chainstate/ -r
+echo "Blockchain removed"
 
-echo "Old files removed"
+echo "Downloading Bootstrap file"
+wget https://idea-01.insufficient-light.com/data/thought-chain.tar.gz
+#wget http://192.168.250.65/thought-chain.tar.gz
+
+echo "Extracting Bootstrap"
+tar -zxf thought-chain.tar.gz
+
 echo "Rebuilding blockchain"
 mv /root/evodb/ /root/.thoughtcore/evodb/
 mv /root/blocks/ /root/.thoughtcore/blocks/
