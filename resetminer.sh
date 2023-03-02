a97f68051490a7207f4000d4d53dbc9b610b7c4f
martinhilgenkamp <31311307+martinhilgenkamp@users.noreply.github.com>
2023-03-02 08:57:43 +0100

    Update resetminer.sh
    Fixed Colorization

--- resetminer.sh
+++ resetminer.sh
@@ -4,36 +4,84 @@ if [ "$EUID" -ne 0 ]
   exit
 fi
 
-GRN='\e[32m'
-CYN='\e[36m'
-END='\e[0m'
+set +x
+function black(){
+    echo -e "\x1B[30m $1 \x1B[0m"
+    if [ ! -z "${2}" ]; then
+    echo -e "\x1B[30m $($2) \x1B[0m"
+    fi
+}
+function red(){
+    echo -e "\x1B[31m $1 \x1B[0m"
+    if [ ! -z "${2}" ]; then
+    echo -e "\x1B[31m $($2) \x1B[0m"
+    fi
+}
+function green(){
+    echo -e "\x1B[32m $1 \x1B[0m"
+    if [ ! -z "${2}" ]; then
+    echo -e "\x1B[32m $($2) \x1B[0m"
+    fi
+}
+function yellow(){
+    echo -e "\x1B[33m $1 \x1B[0m"
+    if [ ! -z "${2}" ]; then
+    echo -e "\x1B[33m $($2) \x1B[0m"
+    fi
+}
+function blue(){
+    echo -e "\x1B[34m $1 \x1B[0m"
+    if [ ! -z "${2}" ]; then
+    echo -e "\x1B[34m $($2) \x1B[0m"
+    fi
+}
+function purple(){
+    echo -e "\x1B[35m $1 \x1B[0m \c"
+    if [ ! -z "${2}" ]; then
+    echo -e "\x1B[35m $($2) \x1B[0m"
+    fi
+}
+function cyan(){
+    echo -e "\x1B[36m $1 \x1B[0m"
+    if [ ! -z "${2}" ]; then
+    echo -e "\x1B[36m $($2) \x1B[0m"
+    fi
+}
+function white(){
 
+    echo -e "\x1B[37m $1 \x1B[0m"
+    if [ ! -z "${2}" ]; then
+    echo -e "\x1B[33m $($2) \x1B[0m"
+    fi
+}
 cd /root
 
-echo "${GRN}Cleaning up${END}"
+green "Cleaning up"
 rm *.tar.gz -rf
 
-echo "${GRN}Stopping services${END}"
+red "Stopping services"
 systemctl stop thoughtd
-echo "${GRN}Removing chain Files${END}"
+green "Removing chain Files"
 rm /root/.thoughtcore/evodb/ -r
 rm /root/.thoughtcore/blocks/ -r
 rm /root/.thoughtcore/chainstate/ -r
-echo "${GRN}Blockchain removed${END}"
+green "Blockchain removed"
 
-echo "${GRN}Downloading Bootstrap file${END}"
+green "Downloading Bootstrap file"
 wget https://idea-01.insufficient-light.com/data/thought-chain.tar.gz
 #wget http://192.168.250.65/thought-chain.tar.gz
 
-echo "${GRN}Extracting Bootstrap${END}"
+green "Extracting Bootstrap"
 tar -zxf thought-chain.tar.gz
 
-echo "${GRN}Rebuilding blockchain${END}"
+green "Rebuilding blockchain"
 mv /root/evodb/ /root/.thoughtcore/evodb/
 mv /root/blocks/ /root/.thoughtcore/blocks/
 mv /root/chainstate/ /root/.thoughtcore/chainstate/
-echo "${GRN}Blockchain recovered{END}"
-echo "${GRN}Starting Thoughtd Service{END}"
+green "Blockchain recovered{END}"
+green "Starting Thoughtd Service{END}"
 systemctl start thoughtd
-echo "${GRN}Removing bootstrap file${END}"
+red "Removing bootstrap file"
 rm /root/thought-chain.tar.gz -f
+green "--------------------------"
+green "success"
