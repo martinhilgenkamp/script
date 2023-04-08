d06ca87735f2580b50337852e86dc803de0e40a4
martinhilgenkamp <31311307+martinhilgenkamp@users.noreply.github.com>
2023-04-08 15:58:48 +0200

    Update resetminer.sh

--- resetminer.sh
+++ resetminer.sh
@@ -56,6 +56,17 @@ function white(){
 }
 cd /root
 
+read -t 5 -p "Lokale chain downloaden? [y/n]" yn
+case $yn in
+     [yY][eE][sS]|[yY])
+     echo "Lokale chain word gedownload"
+     wget http://192.168.250.167/thought-chain.tar.gz
+;;
+     [nN][oO]|[nN])
+     echo "Online chain word gedownload"
+     wget https://idea-01.insufficient-light.com/data/thought-chain.tar.gz
+;;
+
 green "Cleaning up"
 rm *.tar.gz -rf
 
@@ -68,7 +79,7 @@ rm /root/.thoughtcore/chainstate/ -r
 green "Blockchain removed"
 
 green "Downloading Bootstrap file"
-wget https://idea-01.insufficient-light.com/data/thought-chain.tar.gz
+
 #wget http://192.168.250.65/thought-chain.tar.gz
 
 green "Extracting Bootstrap"
