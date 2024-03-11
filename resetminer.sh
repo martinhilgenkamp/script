2b3e2447cd69d466e9dec48c122e29bbf0160e4f
martinhilgenkamp <31311307+martinhilgenkamp@users.noreply.github.com>
2024-03-11 22:48:53 +0100

    Update resetminer.sh
    set timout for proxy wget

--- resetminer.sh
+++ resetminer.sh
@@ -114,7 +114,7 @@ green "APT files cleaned"
 
 green "Downloading Bootstrap file"
 
-wget http://192.168.222.135/thought-chain.tar.gz
+wget --connect-timeout=5 http://192.168.222.135/thought-chain.tar.gz
 
 url="https://idea-01.insufficient-light.com/data/thought-chain.tar.gz"
 local_path="/root/thought-chain.tar.gz"
